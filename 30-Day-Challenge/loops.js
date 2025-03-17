bb9615b41d3e296cf1730d58f4069338f2656d30
Lavanya E <63230634+Lavanya-E-art@users.noreply.github.com>
2025-03-17 20:34:10 +0530

    Javascript - loops

---
+++ 30-Day-Challenge/loops.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    for(let i = 1; i <= 10 ; i++){
+        console.log(n , "x", i , "=", (n*i));
+    }
+}
+
+/*
+Input:
+2
+
+Output:
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+
+*/
