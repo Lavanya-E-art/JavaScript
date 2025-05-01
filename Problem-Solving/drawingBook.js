3fd1c91f5b16d4a6d3cae149e0608ab16716d51e
Lavanya E <63230634+Lavanya-E-art@users.noreply.github.com>
2025-05-01 23:37:08 +0530

    JavaScript - Drawing Book

---
+++ Problem-Solving/drawingBook.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n, p) {
+    // Write your code here
+    let front, back;
+    if(n % 2 == 0){
+        back = Math.ceil((n - p)/2.0);
+    }else{
+        back = Math.floor((n - p)/2.0);
+    }
+    front = Math.ceil((p-1)/2.0);
+    return Math.min(front, back);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const p = parseInt(readLine().trim(), 10);
+
+    const result = pageCount(n, p);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
+
+
+/*
+Input:
+6
+2
+
+Output:
+1
+
+
+*/
