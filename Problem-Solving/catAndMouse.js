3ad80f75376feb40c071dcc5478ddd76ccdbf873
Lavanya E <63230634+Lavanya-E-art@users.noreply.github.com>
2025-05-02 15:34:41 +0530

    JavaScript - Cat And Mouse

---
+++ Problem-Solving/catAndMouse.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the catAndMouse function below.
+function catAndMouse(x, y, z) {
+    let aPos = Math.abs(z - x);
+    let bPos = Math.abs(z - y);
+    if(aPos < bPos){
+        return "Cat A";
+    }else if(aPos > bPos){
+        return "Cat B";
+    }else if(aPos == bPos){
+        return "Mouse C";
+    }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const xyz = readLine().split(' ');
+
+        const x = parseInt(xyz[0], 10);
+
+        const y = parseInt(xyz[1], 10);
+
+        const z = parseInt(xyz[2], 10);
+
+        let result = catAndMouse(x, y, z);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
+
+/*
+Input:
+2
+1 2 3
+1 3 2
+
+Output:
+Cat B
+Mouse C
+
+
+*/
