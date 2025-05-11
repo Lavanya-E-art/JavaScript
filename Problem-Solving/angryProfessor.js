6d0e08b169f493c28656539a514c765d505b9e6e
Lavanya E <63230634+Lavanya-E-art@users.noreply.github.com>
2025-05-11 08:50:11 +0530

    JavaScript - Angry Professor

---
+++ Problem-Solving/angryProfessor.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'angryProfessor' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY a
+ */
+
+function angryProfessor(k, a) {
+    // Write your code here
+    let negCount = 0, posCount = 0;
+    for(let i = 0 ; i < a.length ; i++){
+        a[i] <= 0 ? negCount += 1 : posCount += 1;
+    }
+    if(negCount >= k){
+        return "NO";
+    }else{
+        return "YES";
+    }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const n = parseInt(firstMultipleInput[0], 10);
+
+        const k = parseInt(firstMultipleInput[1], 10);
+
+        const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+        const result = angryProfessor(k, a);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
+
+/*
+Input:
+2
+4 3
+-1 -3 4 2
+4 2
+0 -1 2 1
+
+Output:
+YES
+NO
+
+
+*/
