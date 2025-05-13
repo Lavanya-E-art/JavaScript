1c8a1b2b343cb81292fb3d70cd046b30bcbce7cf
Lavanya E <63230634+Lavanya-E-art@users.noreply.github.com>
2025-05-13 17:12:23 +0530

    JavaScript - Jumping On Clouds

---
+++ Problem-Solving/jumpingOnClouds.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c, k) {
+        let e =100;
+        let n = c.length;
+        let i =0;
+        do{
+            i = (i+k)%n;
+            e--;
+            if(c[i]==1)
+                e-=2;     
+        }while(i!=0);
+        return e;
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
+
+/*
+Input
+8 2
+0 0 1 0 0 1 1 0
+
+Output:
+92
+
+
+*/
