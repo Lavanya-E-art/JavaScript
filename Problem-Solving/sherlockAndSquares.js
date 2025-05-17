176102463230e400fdee34465168e142e5a029b6
Lavanya E <63230634+Lavanya-E-art@users.noreply.github.com>
2025-05-17 15:36:18 +0530

    JavaScript - Sherlock And Squares

---
+++ Problem-Solving/sherlockAndSquares.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'squares' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER a
+ *  2. INTEGER b
+ */
+
+function squares(a, b) {
+    // Write your code here
+    let c = 0;
+    
+    let n = Math.ceil(Math.sqrt(a));
+        
+    while(n * n <= b) {
+        c += 1;
+        n += 1;
+    }
+        
+    return c;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const a = parseInt(firstMultipleInput[0], 10);
+
+        const b = parseInt(firstMultipleInput[1], 10);
+
+        const result = squares(a, b);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
+
+/*
+Input:
+2
+3 9
+17 24
+
+Output:
+2
+0
+
+
+*/
